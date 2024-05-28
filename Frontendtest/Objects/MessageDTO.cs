95fbaa47178f93414c13bb657aa03b1ff58bf664
Rick Janssen <I454010@fhict.nl>
2024-05-28 11:53:55 +0200

    Added authenticates calls

---
+++ Frontendtest/Objects/MessageDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Frontendtest.Objects
+{
+    public class MessageDTO
+    {
+        public int Id { get; set; }
+        public string Group { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
--- Frontendtest/Program.cs
+++ Frontendtest/Program.cs
@@ -1,4 +1,6 @@
 using Auth0.AspNetCore.Authentication;
+using Frontendtest;
+using Frontendtest.Objects;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -10,10 +12,24 @@ builder.Services.AddAuth0WebAppAuthentication(options =>
 {
     options.Domain = builder.Configuration["Auth0:Domain"];
     options.ClientId = builder.Configuration["Auth0:ClientId"];
+    options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+}).WithAccessToken(options =>
+{
+    options.Audience = builder.Configuration["Auth0:Audience"];
 });
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<TokenHandler>();
+
+builder.Services.AddHttpClient("ExternalAPI",
+      client => client.BaseAddress = new Uri(builder.Configuration["ExternalApiBaseUrl"]))
+      .AddHttpMessageHandler<TokenHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+  .CreateClient("ExternalAPI"));
 
 var app = builder.Build();
 
@@ -41,5 +57,11 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+app.MapGet("/api/Message", async (HttpClient httpClient) =>
+{
+    return await httpClient.GetFromJsonAsync<MessageDTO[]>("Message");
+})
+.RequireAuthorization();
 
 app.Run();
